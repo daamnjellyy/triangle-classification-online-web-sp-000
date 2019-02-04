b4c101c5c59b86bde7c6074dee638681d0e08959
DANIELLE PARK <dpark1008@gmail.com>
2019-02-04 00:50:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,14 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
 
-  def initialize
+  def initialize(side1, side2, side3)
+    @triangle_sides = []
+    @triangle_sides << side1
+    @triangle_sides << side2
+    @triangle_sides << side3
   end
+
+  def valid?
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
 end
 
 class TriangleError < StandardError
